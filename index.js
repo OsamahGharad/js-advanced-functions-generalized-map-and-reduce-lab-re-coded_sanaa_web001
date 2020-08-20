7150e558976ed4f11130a48019435f0b28233eb4
Osamah Gharad <osamah.gharred@gmail.com>
2020-08-20 21:27:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 // Add your functions here
-
+// Gneralized mapToNegativize return nagative values of a given array
 function mapToNegativize(numbers,func){
   return numbers.map(func);
+}
+
+//mapToNoChange  returnsame values of a given array
+function mapToNoChange(array,func){
+  const newArray=[];
+   array.forEach(element =>newArray.push(func(element)));
+   return newArray;
 }
